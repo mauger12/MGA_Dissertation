8e52f6fac9c1ff252f7f2a1bb30866ca1e51605f
mauger12 <matthew.auger12@gmail.com>
2023-03-01 15:49:00 +0000

    offline_cpd function worked on, ran using test data set but is inaccurate

--- main.py
+++ main.py
@@ -32,14 +32,32 @@ analyse data generated
 """
 import pandas as pd
 import matplotlib.pyplot as plt
+import ruptures as rpt
 
 
 def load_single_dataset(path):
     df = pd.read_csv(path)
-    plt.plot(df['ts'].loc[:100], df['E'].loc[:100])
+    plt.plot(df['time'], df['value'])
+    plt.show()
+    return df['value'].values
+
+
+def offline_cpd(signal):
+    # generate signal
+    # samples is num of data points, dim is dimensions - each column of power is a dim, sigma is deviation in signal
+    # n_samples, dim, sigma = 1000, 1, 2
+    # n_bkps = 4  # number of breakpoints
+    # signal, bkps = rpt.pw_constant(n_samples, dim, noise_std=sigma)
+
+    # detection
+    algo = rpt.Pelt(model="rbf").fit(signal)
+    result = algo.predict(pen=3)
+    # display
+    rpt.display(signal, result)
     plt.show()
 
 
 if __name__ == '__main__':
-    path = 'QSR_dataset\\QSR3\\466e\\466e0001-20200220-20200712-60Min-analytics.csv'
-    load_single_dataset(path)
+    path = 'data1.csv'  # 'QSR_dataset\\QSR3\\466e\\466e0001-20200220-20200712-60Min-analytics.csv'
+    data = load_single_dataset(path)
+    offline_cpd(data)
